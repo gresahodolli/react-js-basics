10b039c72d04519868c2ed7b9fb55ec986a281b4
gresahodolli <gresa.hodolli1@student.uni-pr.edu>
2023-10-20 13:07:34 +0200

    props

--- react-js-basics-2/src/components/Greet.js
+++ react-js-basics-2/src/components/Greet.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-// function Greet(){
-//     return <h1>Hello Pikachu</h1>
-// }
-
-const Greet = ()=> <h1>Hello Pikachu</h1>
+const Greet = (props)=> {
+    console.log(props)
+    props.name="Pika pika"
+    return(
+    <div>
+        <h1>Hello {props.name} a.k.a {props.heroName}</h1>
+        {props.children}
+    </div>
+    )
+}
 
 export default Greet
--- react-js-basics-2/src/components/Hello.js
+++ react-js-basics-2/src/components/Hello.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hello = () => {
     // return (
-    //     <div>
+    //     <div class='dummyClass'>
     //         <h1>Hello Pikachu</h1>
     //     </div>
     // )
