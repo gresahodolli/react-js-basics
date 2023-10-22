f339549a5a6395d17f8c1b189ef30f54632d1ee0
gresahodolli <gresa.hodolli1@student.uni-pr.edu>
2023-10-22 23:05:55 +0200

    callback refs

--- react-js-basics-2/src/components/RefsDemo.js
+++ react-js-basics-2/src/components/RefsDemo.js
@@ -4,11 +4,18 @@ class RefsDemo extends Component {
     constructor(props) {
       super(props)
       this.inputRef=React.createRef()
+      this.cbRef=null
+      this.setCbRef=(element)=>{
+        this.cbRef=element
+      }
     }
 
     componentDidMount(){
-        this.inputRef.current.focus()
-        console.log(this.inputRef)
+        if(this.cbRef){
+            this.cbRef.focus()
+        }
+        // this.inputRef.current.focus()
+        // console.log(this.inputRef)
     }
     clickHandler=() => {
         alert(this.inputRef.current.value)
@@ -18,6 +25,7 @@ class RefsDemo extends Component {
     return (
       <div>
         <input type="text" ref={this.inputRef}></input>
+        <input type="text" ref={this.setCbRef}></input>
         <button onClick={this.clickHandler}>Click</button>
       </div>
     )
