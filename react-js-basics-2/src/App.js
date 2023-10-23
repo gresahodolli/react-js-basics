fb6594675eb9828d6a9193391d1aaeb0800985b9
gresahodolli <gresa.hodolli1@student.uni-pr.edu>
2023-10-23 22:00:22 +0200

    higher order components / implement HoverCounter

--- react-js-basics-2/src/App.js
+++ react-js-basics-2/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css'
 import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
 
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className='App'>
         <ClickCounter></ClickCounter>
+        <HoverCounter></HoverCounter>
       </div>
     );
   }
