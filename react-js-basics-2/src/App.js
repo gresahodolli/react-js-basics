24d90532bfc5508bca5e455f0f435282c772d962
gresahodolli <gresa.hodolli1@student.uni-pr.edu>
2023-10-23 21:23:21 +0200

    error boundary

--- react-js-basics-2/src/App.js
+++ react-js-basics-2/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-
-import PortalDemo from './components/PortalDemo';
+import './App.css'
+import Hero from './components/Hero';
+import ErrorBoundary from './components/ErrorBoundary';
 
 class App extends Component {
   render() {
     return (
       <div>
-         <PortalDemo />
+        <ErrorBoundary>
+          <Hero heroName ="Batman"></Hero>
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <Hero heroName="Superman"></Hero>
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <Hero heroName="Joker"></Hero>
+        </ErrorBoundary>
       </div>
     );
   }
