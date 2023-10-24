ebb25c057f78ec6867f37fa08f89d79c6b52eae9
gresahodolli <gresa.hodolli1@student.uni-pr.edu>
2023-10-24 21:26:01 +0200

    context 1

--- react-js-basics-2/src/App.js
+++ react-js-basics-2/src/App.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
 import './App.css'
-import ClickCounterTwo from './components/ClickCounterTwo';
-import Counter1 from './components/Counter1';
-import HoverCounterTwo from './components/HoverCounterTwo';
-import User from './components/User';
+import ComponentC from './components/ComponentC'
+import { UserProvider } from './components/UserContext';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        {/* Uncomment these lines if you want to use these components */}
-        {/* <ClickCounterTwo />
-        <HoverCounterTwo />
-        <User render={(IsLoggedIn) => IsLoggedIn ? 'Arberesha' : 'Guest'} /> */}
-
-        <Counter1 render={(count, incrementCount) => <ClickCounterTwo count={count} incrementCount={incrementCount} />} />
-        <HoverCounterTwo render={(count, incrementCount) => <ClickCounterTwo count={count} incrementCount={incrementCount} />} />
-      </div>
+        <UserProvider value="Gresa">
+        <ComponentC />
+        </UserProvider>
+       </div>
     );
   }
 }
