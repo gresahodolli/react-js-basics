f24bb16995bee3595ac8a36468c1376e6b73815e
gresahodolli <gresa.hodolli1@student.uni-pr.edu>
2023-10-24 20:53:13 +0200

    render props 1

--- react-js-basics-2/src/App.js
+++ react-js-basics-2/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './App.css'
-import ClickCounter from './components/ClickCounter';
-import HoverCounter from './components/HoverCounter';
+import ClickCounterTwo from './components/ClickCounterTwo';
+import HoverCounterTwo from './components/HoverCounterTwo';
 
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <ClickCounter ></ClickCounter>
-        <HoverCounter ></HoverCounter>
+        <ClickCounterTwo ></ClickCounterTwo>
+        <HoverCounterTwo ></HoverCounterTwo>
       </div>
     );
   }
