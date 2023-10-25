1b094c14936c1b56c56c02c5cf2f081f753cc758
gresahodolli <gresa.hodolli1@student.uni-pr.edu>
2023-10-25 22:37:10 +0200

    Create PostForm

---
+++ react-http/src/components/PostForm.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+class PostForm extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+        userId:'',
+        title: '',
+        body: ''
+      }
+    }
+
+    changeHandler = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+    
+    submitHandler = e => {
+        e.preventDefault()
+        console.log(this.state)
+    }
+
+  render() {
+    const {userId, title, body}=this.state
+    return (
+      <div>
+        <form onSubmit={this.submitHandler}>
+        <div>
+            <input type="text" name="userId" value={userId} onChange={this.changeHandler}></input>
+        </div>
+        <div>
+            <input type="text" name="title" value={title} onChange={this.changeHandler}></input>
+        </div>
+        <div>
+            <input type="text" name="body" value={body} onChange={this.changeHandler}></input>
+        </div>
+        <button type="submit"></button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default PostForm
