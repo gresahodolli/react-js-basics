0cde5262b12dd88e0551f2a33358df3f8cf9dc9c
gresahodolli <gresa.hodolli1@student.uni-pr.edu>
2023-10-26 20:43:56 +0200

    create ClassBased Counter

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import ClassCounter from './components/ClassCounter';
 
 function App() {
   return (
     <div className="App">
-      
+      <ClassCounter />
     </div>
   );
 }
