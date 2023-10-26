43d2429b8e628c50fff0e9924886397d2d18ef15
gresahodolli <gresa.hodolli1@student.uni-pr.edu>
2023-10-26 21:46:05 +0200

    UseState with object

---
+++ state-hook/src/components/HookCounterThree.js
@@ -0,0 +1,16 @@
+import React,{useState} from 'react'
+
+function HookCounterThree() {
+
+    const[name,setName] =useState({firstName: '', lastName: ''})
+  return (
+    <div>
+        <input type="text" value={name.firstName} onChange={e => setName({firstName: e.target.value})}></input>
+        <input type="text" value={name.lastName} onChange={e => setName({lastName: e.target.value})}></input>
+        <h2>Your first name is - {name.firstName}</h2>
+        <h2>Your last name is - {name.lastName}</h2>
+    </div>
+  )
+}
+
+export default HookCounterThree
