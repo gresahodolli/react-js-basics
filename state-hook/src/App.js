22d7e28a80c997605812cad7d590ba63dec1a2c7
gresahodolli <gresa.hodolli1@student.uni-pr.edu>
2023-10-26 20:54:31 +0200

    UseState Hook

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
 
 function App() {
   return (
     <div className="App">
-      <ClassCounter />
+      <HookCounter></HookCounter>
     </div>
   );
 }
