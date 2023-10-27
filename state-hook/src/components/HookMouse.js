ac9beda026b363eeca96c5507739cfada0779c13
gresahodolli <gresa.hodolli1@student.uni-pr.edu>
2023-10-27 17:29:54 +0200

    useEffect with cleanup

--- state-hook/src/components/HookMouse.js
+++ state-hook/src/components/HookMouse.js
@@ -14,6 +14,7 @@ function HookMouse() {
 
     
     return () => {
+      console.log('Component unmounting code')
       window.removeEventListener('mousemove', logMousePosition);
     };
   }, []); 
