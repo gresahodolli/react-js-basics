dd96fc58a84061ece18b1d94b93b32a5097b5e9f
gresahodolli <gresa.hodolli1@student.uni-pr.edu>
2023-10-27 16:45:21 +0200

    useEffect after render

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import HookCounterFour from './components/HookCounterFour';
+import ClassCounterOne from './components/ClassCounterOne';
+import HookCounterOne from './components/HookCounterOne';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterFour></HookCounterFour>
+      {/* <ClassCounterOne></ClassCounterOne> */}
+      <HookCounterOne></HookCounterOne>
     </div>
   );
 }
