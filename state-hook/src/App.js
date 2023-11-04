3a815b27ef99f6226e412a57a2b20ca5842d1520
gresahodolli <gresa.hodolli1@student.uni-pr.edu>
2023-11-04 19:56:41 +0100

    useContext Hook

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
 import DataFetching from './components/DataFetching';
+import ComponentC from './components/ComponentC';
+import React from 'react';
+
+export const UserContext = React.createContext()
+export const ChannelContext=React.createContext()
 
 function App() {
   return (
     <div className="App">
-      <DataFetching />
+      <UserContext.Provider value={'Gresa'}>
+        <ChannelContext.Provider value={'react-js-basics'}>
+         <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider>   
     </div>
   );
 }
