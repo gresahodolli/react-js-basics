0fa57d6bf04862efdbe880f06fd6f6d2cb07c169
gresahodolli <gresa.hodolli1@student.uni-pr.edu>
2023-11-05 14:09:38 +0100

    useReducer (complex state & action)

--- reducer-hook/src/App.js
+++ reducer-hook/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import CounterOne from './components/CounterOne';
+import CounterTwo from './components/CounterTwo';
 
 function App() {
   return (
     <div className="App">
-      <CounterOne />
+      <CounterTwo />
     </div>
   );
 }
